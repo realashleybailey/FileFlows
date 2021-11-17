3ff7fa075ed1f45fe2ad64fd7bf03725cf49d1ef
reven <john@reven.nz>
2021-11-18 08:35:54 +1300

    Update delete source directory to check if directory is empty

--- Plugins/BasicNodes/File/DeleteSourceDirectory.cs
+++ Plugins/BasicNodes/File/DeleteSourceDirectory.cs
@@ -8,9 +8,15 @@ namespace FileFlow.BasicNodes.File
     public class DeleteSourceDirectory : Node
     {
         public override int Inputs => 1;
-        public override int Outputs => 1;
+        public override int Outputs => 2;
         public override FlowElementType Type => FlowElementType.Process;
         public override string Icon => "far fa-trash-alt";
+
+        [Boolean(1)]
+        public bool IfEmpty { get; set; }
+
+        [StringArray(2)]
+        public string[] IncludePatterns { get; set; }
 
         public override int Execute(NodeParameters args)
         {
@@ -25,6 +31,41 @@ namespace FileFlow.BasicNodes.File
 
             string topdir = args.RelativeFile.Substring(0, pathIndex);
             string pathToDelete = Path.Combine(path, topdir);
+
+            if (IfEmpty)
+            {
+                var files = new System.IO.DirectoryInfo(pathToDelete).GetFiles("*.*", SearchOption.AllDirectories);
+                if (IncludePatterns?.Any() == true)
+                {
+                    var count = files.Where(x =>
+                    {
+                        foreach (var pattern in IncludePatterns)
+                        {
+                            if (x.FullName.Contains(pattern))
+                                return true;
+                            try
+                            {
+                                if (System.Text.RegularExpressions.Regex.IsMatch(x.FullName, pattern, System.Text.RegularExpressions.RegexOptions.IgnoreCase))
+                                    return true;
+                            }
+                            catch (Exception) { }
+                        }
+                        return false;
+                    }).Count();
+                    if (count > 0)
+                    {
+                        args.Logger.ILog("Directory is not empty, cannot delete: " + pathToDelete);
+                        return 2;
+                    }
+                }
+                else if (files.Length == 0)
+                {
+                    args.Logger.ILog("Directory is not empty, cannot delete: " + pathToDelete);
+                    return 2;
+                }
+            }
+
+
             args.Logger.ILog("Deleting directory: " + pathToDelete);
             try
             {

02e5dfcab906fe8c034ed88c44bffd2daf4ad714
reven <john@reven.nz>
2021-12-20 19:34:02 +1300

    and still

--- build/build-server.ps1
+++ build/build-server.ps1
@@ -47,9 +47,9 @@ if ((Test-Path deploy\plugins) -eq $true) {
 
 if ( $linux -eq $false) {
     
-    (Get-Content WindowsServerInstaller\Program.cs) -replace '0.0.0.0', "$version" | Out-File  WindowsServerInstaller\Program.cs -Encoding ascii
+    (Get-Content build\installers\FileFlowInstallers\WindowsServerInstaller\Program.cs) -replace '0.0.0.0', "$version" | Out-File  build\installers\FileFlowInstallers\WindowsServerInstaller\Program.cs -Encoding ascii
     $curDir = Get-Location
-    (Get-Content WindowsServerInstaller\Program.cs) -replace 'C\:\\Users\\john\\src\\FileFlows\\FileFlows\\deploy\\FileFlows', "$curDir\deploy" | Out-File  WindowsServerInstaller\Program.cs -Encoding ascii
+    (Get-Content build\installers\FileFlowInstallers\WindowsServerInstaller\Program.cs) -replace 'C\:\\Users\\john\\src\\FileFlows\\FileFlows\\deploy\\FileFlows', "$curDir\deploy" | Out-File  build\installers\FileFlowInstallers\WindowsServerInstaller\Program.cs -Encoding ascii
 
     if (Test-Path -Path 'C:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools\MSBuild\Current\Bin\msbuild.exe' -PathType Leaf) {
         & 'C:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools\MSBuild\Current\Bin\msbuild.exe' build\installers\FileFlowInstallers.sln

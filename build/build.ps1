d0e5b24bf3531b2c35d3e068aef9deba35adc567
reven <john@reven.nz>
2021-12-29 12:18:10 +1300

    adding error to build script

--- build/build.ps1
+++ build/build.ps1
@@ -1,7 +1,7 @@
 $revision = (git rev-list --count --first-parent HEAD) -join "`n"
 $version = "0.1.0.$revision"
 if ((Test-Path ../deploy/Plugins) -eq $false) {
-    Write-Error "No plugins directory found"
+    Write-Error "ERROR: No plugins directory found"
     return
 }
 

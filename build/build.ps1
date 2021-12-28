f9092b10f1f62607ebd3c9252e41f3f9b248ac00
reven <john@reven.nz>
2021-12-29 11:49:43 +1300

    removing plugins from build script

--- build/build.ps1
+++ build/build.ps1
@@ -4,7 +4,7 @@ if (Test-Path ..\deploy) {
 $revision = (git rev-list --count --first-parent HEAD) -join "`n"
 $version = "0.1.0.$revision"
 
-.\build-plugins.ps1
+#.\build-plugins.ps1
 .\build-spellcheck.ps1
 .\build-flowrunner.ps1
 .\build-server.ps1
@@ -21,3 +21,6 @@ Compress-Archive @compress
 
 Remove-Item ..\deploy\FileFlows-Node -Recurse -ErrorAction SilentlyContinue 
 Remove-Item ..\deploy\FileFlows -Recurse -ErrorAction SilentlyContinue 
+
+# no longer need plugins, delete them
+Remove-Item ..\deploy\Plugins -Recurse -ErrorAction SilentlyContinue 

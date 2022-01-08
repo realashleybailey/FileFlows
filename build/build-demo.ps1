634557544d047f961e1f217b03ea6a0e8471e192
reven <john@reven.nz>
2022-01-08 22:18:12 +1300

    trying to fix demo build

--- build/build-demo.ps1
+++ build/build-demo.ps1
@@ -14,8 +14,9 @@ Push-Location ..\
 (Get-Content Client\Client.csproj) -replace '<Version>[^<]+</Version>', "<Version>$version</Version>" | Out-File Client\Client.csproj
 (Get-Content Client\Client.csproj) -replace '<ProductVersion>[^<]+</ProductVersion>', "<ProductVersion>$version</ProductVersion>" | Out-File Client\Client.csproj
 (Get-Content Client\Client.csproj) -replace '<Copyright>[^<]+</Copyright>', "<Copyright>$copyright</Copyright>" | Out-File Client\Client.csproj
+(Get-Content Client\Client.csproj) -replace '<DefineConstants>[^<]+</DefineConstants>', "<DefineConstants>$$(DefineConstants)TRACE;DEMO</DefineConstants>" | Out-File Client\Client.csproj
 
-dotnet.exe publish Client\Client.csproj --configuration Release --output $outdir /p:AssemblyVersion=$version /p:Version=$version /p:CopyRight=$copyright /p:ExtraDefineConstants=DEMO
+dotnet.exe publish Client\Client.csproj --configuration Release --output $outdir /p:AssemblyVersion=$version /p:Version=$version /p:CopyRight=$copyright /p:DefineConstants=DEMO
 
 (Get-Content $outdir\wwwroot\index.html) -replace ' && location.hostname !== ''localhost''', '' | Out-File $outdir\wwwroot\index.html -encoding ascii
 Remove-Item $outdir\wwwroot\_Framework\*.dll -Recurse -ErrorAction SilentlyContinue 

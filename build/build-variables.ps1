9a896f492e9e6465ac96f5315afc5efe26800509
reven <john@reven.nz>
2022-01-14 17:19:52 +1300

    updated version number

--- build/build-variables.ps1
+++ build/build-variables.ps1
@@ -1,4 +1,4 @@
 $revision = (git rev-list --count --first-parent HEAD) -join "`n"
-$version = "0.3.1.$revision"
+$version = "0.3.2.$revision"
 $year = (Get-Date).year
 $copyright = "Copyright $year - John Andrews"

fffd3cbed18a5a229a7e5609c9253b31b34ff2c7
reven <john@reven.nz>
2022-01-16 14:00:32 +1300

    updated build script

--- build/build-installer.ps1
+++ build/build-installer.ps1
@@ -1,7 +1,8 @@
 $sln = $args[0]
-$dir = $sln.Substring($sln.lastIndexOf('\'))
+$dir = $sln.Substring(0, $sln.lastIndexOf('\'))
 
 if (Test-Path -Path 'C:\Utils\cs-script\cscs.exe' -PathType Leaf) {    
+    Write-Output "Building intaller with cscs.exe: $dir\Program.cs"
     & 'C:\Utils\cs-script\cscs.exe' "$dir\Program.cs"
 }
 elseif (Test-Path -Path 'C:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools\MSBuild\Current\Bin\msbuild.exe' -PathType Leaf) {        

2290936629fb70e76fa90b80b69f5ca96c117efb
reven <john@reven.nz>
2022-01-16 14:31:24 +1300

    build

--- build/build-installer.ps1
+++ build/build-installer.ps1
@@ -1,5 +1,5 @@
 $builddir = $args[0]
-$programfile = "$dir\installers\WindowsServerInstaller\Program.cs"
+$programfile = "$builddir\installers\WindowsServerInstaller\Program.cs"
 
 Write-Output "Building intaller with cscs.exe: $programfile"
 & "$builddir\utils\wixsharp\cscs.exe" $programfile

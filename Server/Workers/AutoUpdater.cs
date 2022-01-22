0e77d1b889063d35b20b74a640ffc8cb7d8e611c
reven <john@reven.nz>
2022-01-23 12:15:08 +1300

    adding more logging to auto updater

--- Server/Workers/AutoUpdater.cs
+++ Server/Workers/AutoUpdater.cs
@@ -20,11 +20,17 @@
                 Logger.Instance.ILog("AutoUpdater: Creating updates directory: " + UpdateDirectory);
                 Directory.CreateDirectory(UpdateDirectory);
             }
+            else
+            {
+                Logger.Instance.ILog("AutoUpdater: Watch Directory:: " + UpdateDirectory);
+            }
 
             WindowsServerExe = Assembly.GetExecutingAssembly()?.FullName ?? Path.Combine(Directory.GetCurrentDirectory(), "FileFlows.exe");
 
             FileSystemWatcher watcher = new FileSystemWatcher(UpdateDirectory);
             watcher.NotifyFilter =
+                             NotifyFilters.Security |
+                             NotifyFilters.Attributes |
                              NotifyFilters.CreationTime |
                              NotifyFilters.DirectoryName |
                              NotifyFilters.FileName |
@@ -34,6 +40,7 @@
             watcher.Changed += Watcher_Changed;
             watcher.Created += Watcher_Changed;
             watcher.Renamed += Watcher_Changed;
+            watcher.Filter = "*.msi";
             watcher.EnableRaisingEvents = true;
 
             CheckForUpdate();
@@ -46,8 +53,6 @@
 
         private void Watcher_Changed(object sender, FileSystemEventArgs e)
         {
-            if (e.FullPath.EndsWith(".msi") == false)
-                return;
             Logger.Instance.ILog("AutoUpdater: File change detected: " + e.FullPath);
             CheckForUpdate();
         }
@@ -74,16 +79,26 @@
             {
                 var match = rgxVersion.Match(file.Name);
                 if (match.Success == false)
+                {
+                    Logger.Instance.ILog("AutoUpdater: File does not match version regex: " + file.Name);
                     continue;
+                }
 
 #pragma warning disable CS8600 // Converting null literal or possible null value to non-nullable type.
                 Version version;
                 if (Version.TryParse(match.Value, out version) == false)
+                {
+                    Logger.Instance.ILog("AutoUpdater: Failed to parse version: " + match.Value);
                     continue;
+                }
 #pragma warning restore CS8600 // Converting null literal or possible null value to non-nullable type.
 
                 if (version > currentVersion)
                     return (file.FullName, match.Value);
+                else
+                {
+                    Logger.Instance.ILog($"AutoUpdater: Version '{version} less than current '{currentVersion}'");
+                }
             }
             return (string.Empty, string.Empty);
         }

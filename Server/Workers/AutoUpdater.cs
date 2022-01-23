25a927bada58df57e71f08e006d50e2f4788ef3b
reven <john@reven.nz>
2022-01-23 21:04:40 +1300

    bumping version

--- Server/Workers/AutoUpdater.cs
+++ Server/Workers/AutoUpdater.cs
@@ -241,6 +241,7 @@
             sb.AppendLine($"del \"{tempFile}\"");
             File.WriteAllText(tempFile, sb.ToString());
 
+
             Logger.Instance.ILog("AutoUpdater: Windows Server Exe: " + WindowsServerExe);
             Logger.Instance.ILog("AutoUpdater: Starting bat file update: " + tempFile);
 

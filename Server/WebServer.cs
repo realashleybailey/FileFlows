bec2bc4d6308db37e171186127709da207ea93cb
reven <john@reven.nz>
2022-01-23 22:05:07 +1300

    bumping

--- Server/WebServer.cs
+++ Server/WebServer.cs
@@ -122,6 +122,7 @@ namespace FileFlows.Server
             Logger.Instance.ILog("Starting File Flows " + Globals.Version);
             if(Program.Docker)
                 Logger.Instance?.ILog("Running inside docker container");
+
             bool upgraded = args?.Any(x => x == "--upgraded") == true;
             if (upgraded)
                 Logger.Instance?.ILog("Started after upgrade");

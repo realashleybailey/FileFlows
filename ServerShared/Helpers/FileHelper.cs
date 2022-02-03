93fe9efc4dcb4b310a674687a7adc1c480affe13
reven <john@reven.nz>
2022-02-04 09:17:48 +1300

    changing checksum to use bufferedstream

--- ServerShared/Helpers/FileHelper.cs
+++ ServerShared/Helpers/FileHelper.cs
@@ -41,10 +41,8 @@ namespace FileFlows.ServerShared.Helpers
                 if (fileInfo.Length > 100_000_000)
                 {
                     // compute hash on first 100MB to speed it update
-                    var bytes = new byte[100_000_000];                    
-                    using var stream = new FileStream(file, FileMode.Open);
-                    int realLength = stream.Read(bytes, 0, bytes.Length);                                        
-                    hash = hasher.ComputeHash(bytes, 0, realLength);
+                    using var stream = new BufferedStream(File.OpenRead(file), 100_000_000);
+                    hash = hasher.ComputeHash(stream);
                 }
                 else
                 {

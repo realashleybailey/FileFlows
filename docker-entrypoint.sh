06ca9f84df59a20852f0d45f73fc75fa895bea59
John Andrews <john@reven.nz>
2022-04-18 00:12:52 +1200

    fixing entrypoint file

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 if [ "$FFNODE" = 'true' ] || [ "$1" = '--node' ] then   
     printf "Launching node\n"
-    exec dotnet /app/FileFlows.Node.dll
+    exec /root/.dotnet/dotnet FileFlows.Node.dll
 else then
     printf "Launching server\n"
-    exec dotnet /app/FileFlows.Server.dll --urls=http://*:5000 --docker
+    exec /root/.dotnet/dotnet FileFlows.Server.dll --urls=http://*:5000 --docker
 fi

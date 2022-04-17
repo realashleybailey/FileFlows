682279af6dbc010e4db1c83e5510fde74719b32a
John Andrews <john@reven.nz>
2022-04-18 02:54:33 +1200

    node env var

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-if [ "$FFNODE" = 'true' ] | [ "$1" = '--node' ]; then   
-    printf "Launching node"
+if [ -v $FFNODE ] | [ "$1" = '--node' ]; then   
+    printf "Launching node\n"
     exec /root/.dotnet/dotnet FileFlows.Node.dll --docker
 else
-    printf "Launching server"
+    printf "Launching server\n"
     exec /root/.dotnet/dotnet FileFlows.Server.dll --urls=http://*:5000 --docker
 fi

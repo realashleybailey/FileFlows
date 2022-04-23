064f48b0693e392cd998e5323fe57da076cb1daf
John Andrews <john@reven.nz>
2022-04-23 13:41:20 +1200

    updated build-version-variable

--- build-version-variable.ps1
+++ build-version-variable.ps1
@@ -1,3 +1,4 @@
 $version = Get-Content .\version3.txt -Raw 
 $version = $version.Trim()
+Write-Output "Setting env.FF_VERSION to: $version"
 Write-Host "##teamcity[setParameter name='env.FF_VERSION' value='$version']"

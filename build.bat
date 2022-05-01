fd231fb3a7269000b8458238b8faa929ba639b86
John Andrews <john@reven.nz>
2022-05-01 13:57:27 +1200

    build

--- build.bat
+++ build.bat
@@ -4,6 +4,8 @@ REM here we specify the root directory is two paths up, the source FileFlows dir
 CALL :NORMALIZEPATH "."
 SET SRCPATH=%RETVAL%
 
+git rev-list --count HEAD > gitversion.txt
+
 @echo on
 docker build %SRCPATH% -f ./build/builder/Dockerfile --tag fileflows:build
 docker run --rm --name FileFlowsBuild -v %SRCPATH%:/src fileflows:build

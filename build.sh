51c5dd16d704187b12b7afba94337feba96a497c
John Andrews <john@reven.nz>
2022-05-01 13:23:36 +1200

    build

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 SRCPATH=$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
 
-git rev-parse --abbrev-ref HEAD > gitversion.txt
+git rev-list --count HEAD > gitversion.txt
 
 docker build $SRCPATH -f ./build/builder/Dockerfile --tag fileflows:build
 docker run --rm --name FileFlowsBuild -v $SRCPATH:/src fileflows:build

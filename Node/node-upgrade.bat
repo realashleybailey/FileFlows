8d0d331bb462dbc52ba30bc03807032566cd2a9b
John Andrews <john@reven.nz>
2022-05-02 20:26:35 +1200

    adding auto updater

--- Node/node-upgrade.bat
+++ Node/node-upgrade.bat
@@ -8,4 +8,5 @@ for %%a in ("*") do move /y "%%~fa" ..\
 for /d %%a in ("*") do move /y "%%~fa" ..\
 
 cd ..
+rmdir /s NodeUpdate
 start dotnet .\FileFlows.Node.dll
--- Node/node-upgrade.sh
+++ Node/node-upgrade.sh
@@ -1,1 +1,13 @@
-taskkill %1
+#!/usr/bin/env bash
+
+kill %1
+
+rm ../*
+rm -rf ../runtimes
+rm -rf ../FileFlows-Runner
+
+mv * ../
+
+cd ..
+rm -rf NodeUpdate
+start dotnet .\FileFlows.Node.dll

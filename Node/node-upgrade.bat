37e5f2a8ad176599d40b377a39ce3435dc775142
John Andrews <john@reven.nz>
2022-05-04 19:59:52 +1200

    fixed windows node upgrade script

--- Node/node-upgrade.bat
+++ Node/node-upgrade.bat
@@ -1,20 +1,35 @@
+@echo off
+
 IF %1=="UPDATE" GOTO RunUpdate
-start node-upgrade.bat "UPDATE" %1
+copy node-upgrade.bat ..\
+start ..\node-upgrade.bat "UPDATE" %1
 GOTO Done
 
 :RunUpdate
+cd ..
 timeout /t 3
+echo Stopping FileFlows Node if running
 taskkill %2
 
-del ../*.*
-rmdir /s ../runtimes
-rmdir /s ../FileFlows-Runner
+echo.
+echo Removing previous version
+FOR /d %%a IN ("*.*") DO IF /i NOT "%%~nxa"=="NodeUpdate" RD /S /Q "%%a"
+FOR %%a IN ("*") DO IF /i NOT "%%~nxa"=="node-upgrade.bat" DEL "%%a"
 
-for %%a in ("*") do move /y "%%~fa" ..\
-for /d %%a in ("*") do move /y "%%~fa" ..\
+echo.
+echo Copying Node update files
+move NodeUpdate\FileFlows-Runner FileFlows-Runner
+move NodeUpdate\runtimes runtimes
+move NodeUpdate\* .
 
-cd ..
-rmdir /s NodeUpdate
-start dotnet .\FileFlows.Node.dll
+echo.
+echo Deleting NodeUpdate directory
+rmdir /q /s NodeUpdate
+
+echo.
+echo Starting FileFlows Node
+start dotnet FileFlows.Node.dll
+
+if exist node-upgrade.bat start /min del node-upgrade.bat
 
 :Done

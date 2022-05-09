7b657e9705a1ad03a93878da671a434d212dc651
John Andrews <john@reven.nz>
2022-05-10 00:41:57 +1200

    wwwroot

--- Server/WebServer.cs
+++ Server/WebServer.cs
@@ -6,6 +6,7 @@ using System.Runtime.InteropServices;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Hosting;
 
 namespace FileFlows.Server;
@@ -83,8 +84,10 @@ public class WebServer
         var provider = new Microsoft.AspNetCore.StaticFiles.FileExtensionContentTypeProvider();
         provider.Mappings[".br"] = "text/plain";
 
+        var wwwroot = Path.Combine(DirectoryHelper.BaseDirectory, "Server", "wwwroot");
         app.UseStaticFiles(new StaticFileOptions
         {
+            FileProvider = new PhysicalFileProvider(wwwroot),
             ContentTypeProvider = provider,
             OnPrepareResponse = x =>
             {

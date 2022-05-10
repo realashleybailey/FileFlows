a1d5f552e42788bec99ae25f745c84ef7ff5cfe3
John Andrews <john@reven.nz>
2022-05-10 21:38:58 +1200

    node upgrade scripts

--- build/builder/Components/Node.cs
+++ build/builder/Components/Node.cs
@@ -9,7 +9,6 @@ public class Node : Component
         Utils.CopyFile(BuildOptions.SourcePath + "/icon.ico", BuildOptions.TempPath + "/Node");
 
         //MakeInstaller();        
-        Utils.CopyFiles(ProjectDirectory, BuildOptions.TempPath + "/Node", false, "node\\-upgrade\\.(ps1|bat|sh)$");
 
         // we want to make a "Node" directory inside the zip, this is so we keep the directory structure of
         // /FileFlows/Data, /FileFlows/Logs, /FileFlows/Node etc
@@ -20,6 +19,7 @@ public class Node : Component
         Utils.DeleteFiles(BuildOptions.TempPath + "/Node/Node", "run-node.*");
         Utils.CopyFiles(ProjectDirectory, BuildOptions.TempPath + "/Node", false, @"run-node\.(bat|sh)$");
         Utils.CopyFiles(BuildOptions.TempPath + "/FlowRunner", BuildOptions.TempPath + "/Node/FlowRunner");
+        Utils.CopyFiles(ProjectDirectory, BuildOptions.TempPath + "/Node", false, "node\\-upgrade\\.(ps1|bat|sh)$");
 
         Utils.Zip(BuildOptions.TempPath + "/Node", $"{BuildOptions.Output}/FileFlows-Node-{Globals.Version}.zip");
         Utils.DeleteFiles(BuildOptions.TempPath + "/Node", "node-upgrade.*");

befdaaea8bf3d0f81cefdc9864e67affe49553c7
John Andrews <john@reven.nz>
2022-05-10 13:10:20 +1200

    fixing entry point

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,18 +6,18 @@ set -e
 if [[ "$FFNODE" == 'true' || "$FFNODE" == '1' || "$1" = '--node' ]]; then
 
     # check if there is an upgrade to apply
-    if test -f "NodeUpdate/node-upgrade.sh"; then
+    if test -f "/app/node-upgrade.sh"; then
         echo "Upgrade found"
-        echo "exec /root/.dotnet/dotnet FileFlows.Node.dll --docker" > 'NodeUpdate/run-node.sh'
-        chmod +x NodeUpdate/run-node.sh
-        chmod +x NodeUpdate/node-upgrade.sh
-        cd NodeUpdate
+        chmod +x /app/node-upgrade.sh
+        cd /app
         exec node-upgrade.sh docker
     fi
 
     printf "Launching node\n"
-    exec /root/.dotnet/dotnet FileFlows.Node.dll --docker
+    cd /app/Node
+    exec /root/.dotnet/dotnet FileFlows.Node.dll --docker true
 else
     printf "Launching server\n"
+    cd /app/Server
     exec /root/.dotnet/dotnet FileFlows.Server.dll --urls=http://*:5000 --docker
 fi

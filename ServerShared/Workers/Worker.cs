6a8d2bc8dd27d7db646cf9a91cbcdc689da5ec02
John Andrews <john@reven.nz>
2022-05-15 15:01:53 +1200

    added comments to worker class

--- ServerShared/Workers/Worker.cs
+++ ServerShared/Workers/Worker.cs
@@ -1,107 +1,123 @@
-﻿namespace FileFlows.ServerShared.Workers
+﻿namespace FileFlows.ServerShared.Workers;
+
+/// <summary>
+/// A worker that will run at a set schedule
+/// </summary>
+public abstract class Worker
 {
-    public abstract class Worker
+    /// <summary>
+    /// Available schedule types
+    /// </summary>
+    public enum ScheduleType
     {
-        public enum ScheduleType
+        Second,
+        Minute,
+        Hourly,
+        Daily
+    }
+
+    /// <summary>
+    /// Gets or sets the amount seconds between each execution of this worker
+    /// </summary>
+    private int Seconds { get; set; }
+    /// <summary>
+    /// Gets or sets the schedule of this worker
+    /// </summary>
+    private ScheduleType Schedule { get; set; }
+
+    /// <summary>
+    /// Creates an instance of a worker
+    /// </summary>
+    /// <param name="schedule">the type of schedule this worker runs at</param>
+    /// <param name="interval">the interval of this worker</param>
+    protected Worker(ScheduleType schedule, int interval)
+    {
+        Initialize(schedule, interval);
+    }
+
+    protected virtual void Initialize(ScheduleType schedule, int interval)
+    {
+        if (interval < 1)
+            interval = 1;
+
+        if (schedule == ScheduleType.Minute)
+            interval *= 60;
+        if (schedule == ScheduleType.Hourly)
+            interval *= 60 * 60;
+        if (schedule == ScheduleType.Daily)
+            interval *= 60 * 60 * 24;
+
+        this.Schedule = schedule;
+        this.Seconds = interval;
+    }
+
+    static readonly List<Worker> Workers = new List<Worker>();
+
+    private System.Timers.Timer timer;
+
+    /// <summary>
+    /// Start the worker
+    /// </summary>
+    public virtual void Start()
+    {
+        if (timer != null)
         {
-            Second,
-            Minute,
-            Hourly,
-            Daily
+            if (timer.Enabled)
+                return; // already running
+            timer.Start();
         }
-
-        private int Seconds { get; set; }
-        private ScheduleType Schedule { get; set; }
-
-        public Worker(ScheduleType schedule, int interval)
+        else
         {
-            Initialize(schedule, interval);
+            timer = new System.Timers.Timer();
+            timer.Elapsed += TimerElapsed;
+            timer.SynchronizingObject = null;
+            timer.Interval = Seconds * 1_000;
+            timer.AutoReset = true;
+            timer.Start();
         }
+    }
 
-        protected virtual void Initialize(ScheduleType schedule, int interval)
+
+    /// <summary>
+    /// Stop the worker
+    /// </summary>
+    public virtual void Stop()
+    {
+        if (timer == null)
+            return;
+        timer.Stop();
+        timer.Dispose();
+        timer = null;
+    }
+
+    private bool Executing = false;
+
+    private void TimerElapsed(object? sender, System.Timers.ElapsedEventArgs e) => Trigger();
+
+    protected void Trigger()
+    {
+        if (Executing)
+            return; // dont let run twice
+
+        _ = Task.Run(() =>
         {
-            if (interval < 1)
-                interval = 1;
-
-            if (schedule == ScheduleType.Minute)
-                interval *= 60;
-            if (schedule == ScheduleType.Hourly)
-                interval *= 60 * 60;
-            if (schedule == ScheduleType.Daily)
-                interval *= 60 * 60 * 24;
-
-            this.Schedule = schedule;
-            this.Seconds = interval;
-        }
-
-        static readonly List<Worker> Workers = new List<Worker>();
-
-        private System.Timers.Timer timer;
-
-        /// <summary>
-        /// Start the worker
-        /// </summary>
-        public virtual void Start()
-        {
-            if (timer != null)
+            Executing = true;
+            try
             {
-                if (timer.Enabled)
-                    return; // already running
-                timer.Start();
+                Execute();
             }
-            else
+            catch (Exception ex)
             {
-                timer = new System.Timers.Timer();
-                timer.Elapsed += TimerElapsed;
-                timer.SynchronizingObject = null;
-                timer.Interval = Seconds * 1_000;
-                timer.AutoReset = true;
-                timer.Start();
+                Logger.Instance?.ELog($"Error in worker '{this.GetType().Name}': {ex.Message}{Environment.NewLine}{ex.StackTrace}");
             }
-        }
-
-
-        /// <summary>
-        /// Stop the worker
-        /// </summary>
-        public virtual void Stop()
-        {
-            if (timer == null)
-                return;
-            timer.Stop();
-            timer.Dispose();
-            timer = null;
-        }
-
-        private bool Executing = false;
-
-        private void TimerElapsed(object? sender, System.Timers.ElapsedEventArgs e) => Trigger();
-
-        protected void Trigger()
-        {
-            if (Executing)
-                return; // dont let run twice
-
-            _ = Task.Run(() =>
+            finally
             {
-                Executing = true;
-                try
-                {
-                    Execute();
-                }
-                catch (Exception ex)
-                {
-                    Logger.Instance?.ELog($"Error in worker '{this.GetType().Name}': {ex.Message}{Environment.NewLine}{ex.StackTrace}");
-                }
-                finally
-                {
-                    Executing = false;
-                }
-            });
-        }
+                Executing = false;
+            }
+        });
+    }
 
-        protected virtual void Execute()
-        {
-        }
+    protected virtual void Execute()
+    {
     }
 }

90935fd4ee27cb9e8f161ad63249119873b898d0
John Andrews <john@reven.nz>
2022-05-15 20:00:09 +1200

    added comments to SettingsService.cs

--- ServerShared/Services/SettingsService.cs
+++ ServerShared/Services/SettingsService.cs
@@ -1,39 +1,60 @@
-﻿namespace FileFlows.ServerShared.Services
-{
-    using FileFlows.Shared.Helpers;
-    using FileFlows.Shared.Models;
+﻿namespace FileFlows.ServerShared.Services;
 
-    public interface ISettingsService
+using FileFlows.Shared.Helpers;
+using FileFlows.Shared.Models;
+
+/// <summary>
+/// Interface for the Settings service which allows accessing of all the system settings
+/// </summary>
+public interface ISettingsService
+{
+    /// <summary>
+    /// Gets the system settings
+    /// </summary>
+    /// <returns>the system settings</returns>
+    Task<Settings> Get();
+}
+
+/// <summary>
+/// An instance of the Settings Service which allows accessing of the system settings
+/// </summary>
+public class SettingsService : Service, ISettingsService
+{
+
+    /// <summary>
+    /// A loader to load an instance of the Settings
+    /// Note: This is used for unit testing and to mock the settings
+    /// </summary>
+    public static Func<ISettingsService> Loader { get; set; }
+
+    /// <summary>
+    /// Loads an instance of the settings service
+    /// </summary>
+    /// <returns>an instance of the settings service</returns>
+    public static ISettingsService Load()
     {
-        Task<Settings> Get();
+        if (Loader == null)
+            return new SettingsService();
+        return Loader.Invoke();
     }
 
-    public class SettingsService : Service, ISettingsService
+    /// <summary>
+    /// Gets the system settings
+    /// </summary>
+    /// <returns>the system settings</returns>
+    public async Task<Settings> Get()
     {
-
-        public static Func<ISettingsService> Loader { get; set; }
-
-        public static ISettingsService Load()
+        try
         {
-            if (Loader == null)
-                return new SettingsService();
-            return Loader.Invoke();
+            var result = await HttpHelper.Get<Settings>($"{ServiceBaseUrl}/api/settings");
+            if (result.Success == false)
+                throw new Exception("Failed to get settings: " + result.Body);
+            return result.Data;
         }
-
-        public async Task<Settings> Get()
+        catch (Exception ex)
         {
-            try
-            {
-                var result = await HttpHelper.Get<Settings>($"{ServiceBaseUrl}/api/settings");
-                if (result.Success == false)
-                    throw new Exception("Failed to get settings: " + result.Body);
-                return result.Data;
-            }
-            catch (Exception ex)
-            {
-                Logger.Instance?.WLog("Failed to get settings: " + ex.Message);
-                return null;
-            }
+            Logger.Instance?.WLog("Failed to get settings: " + ex.Message);
+            return null;
         }
     }
 }

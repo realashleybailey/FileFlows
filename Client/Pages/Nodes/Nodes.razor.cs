a542cee0fa0768043192496ff9ac53567c855a1c
John Andrews <john@reven.nz>
2022-05-16 21:04:41 +1200

    added option to download the node zip from the web console

--- Client/Pages/Nodes/Nodes.razor.cs
+++ Client/Pages/Nodes/Nodes.razor.cs
@@ -1,92 +1,102 @@
-namespace FileFlows.Client.Pages
+namespace FileFlows.Client.Pages;
+
+using FileFlows.Client.Components;
+
+/// <summary>
+/// Page for processing nodes
+/// </summary>
+public partial class Nodes : ListPage<ProcessingNode>
 {
-    using FileFlows.Client.Components;
+    public override string ApiUrl => "/api/node";
+    const string FileFlowsServer = "FileFlowsServer";
 
-    public partial class Nodes : ListPage<ProcessingNode>
+    private ProcessingNode EditingItem = null;
+
+    private string lblAddress, lblRunners, lblVersion, lblDownloadNode;
+     
+#if(DEBUG)
+    string DownlaodUrl = "http://localhost:6868/api/node/download";
+#else
+    string DownlaodUrl = "/api/node/download";
+#endif
+    protected override void OnInitialized()
     {
-        public override string ApiUrl => "/api/node";
-        const string FileFlowsServer = "FileFlowsServer";
-
-        private ProcessingNode EditingItem = null;
-
-        private string lblAddress, lblRunners, lblVersion;
-        protected override void OnInitialized()
-        {
-            base.OnInitialized();
-            lblAddress = Translater.Instant("Pages.Nodes.Labels.Address");
-            lblRunners = Translater.Instant("Pages.Nodes.Labels.Runners");
-            lblVersion = Translater.Instant("Pages.Nodes.Labels.Version");
-        }
-
-
-        private async Task Add()
-        {
-#if (!DEMO)
-            await Edit(new ProcessingNode());
-#endif
-        }
-
-        public override Task PostLoad()
-        {
-            var serverNode = this.Data?.Where(x => x.Address == FileFlowsServer).FirstOrDefault();
-            if(serverNode != null)
-            {
-                serverNode.Name = Translater.Instant("Pages.Nodes.Labels.FileFlowsServer");                
-            }
-            return base.PostLoad();
-        }
-
-
-        async Task Enable(bool enabled, ProcessingNode node)
-        {
-#if (DEMO)
-            return;
-#else
-            Blocker.Show();
-            try
-            {
-                await HttpHelper.Put<ProcessingNode>($"{ApiUrl}/state/{node.Uid}?enable={enabled}");
-            }
-            finally
-            {
-                Blocker.Hide();
-            }
-#endif
-        }
-
-        async Task<bool> Save(ExpandoObject model)
-        {
-#if (DEMO)
-            return true;
-#else
-            Blocker.Show();
-            this.StateHasChanged();
-
-            try
-            {
-                var saveResult = await HttpHelper.Post<ProcessingNode>($"{ApiUrl}", model);
-                if (saveResult.Success == false)
-                {
-                    Toast.ShowError( saveResult.Body?.EmptyAsNull() ?? Translater.Instant("ErrorMessages.SaveFailed"));
-                    return false;
-                }
-
-                int index = this.Data.FindIndex(x => x.Uid == saveResult.Data.Uid);
-                if (index < 0)
-                    this.Data.Add(saveResult.Data);
-                else
-                    this.Data[index] = saveResult.Data;
-                await this.Load(saveResult.Data.Uid);
-
-                return true;
-            }
-            finally
-            {
-                Blocker.Hide();
-                this.StateHasChanged();
-            }
-#endif
-        }
-
+        base.OnInitialized();
+        lblAddress = Translater.Instant("Pages.Nodes.Labels.Address");
+        lblRunners = Translater.Instant("Pages.Nodes.Labels.Runners");
+        lblVersion = Translater.Instant("Pages.Nodes.Labels.Version");
+        lblDownloadNode = Translater.Instant("Pages.Nodes.Labels.DownloadNode");
+       
     }
+
+
+    private async Task Add()
+    {
+#if (!DEMO)
+        await Edit(new ProcessingNode());
+#endif
+    }
+
+    public override Task PostLoad()
+    {
+        var serverNode = this.Data?.Where(x => x.Address == FileFlowsServer).FirstOrDefault();
+        if(serverNode != null)
+        {
+            serverNode.Name = Translater.Instant("Pages.Nodes.Labels.FileFlowsServer");                
+        }
+        return base.PostLoad();
+    }
+
+
+    async Task Enable(bool enabled, ProcessingNode node)
+    {
+#if (DEMO)
+        return;
+#else
+        Blocker.Show();
+        try
+        {
+            await HttpHelper.Put<ProcessingNode>($"{ApiUrl}/state/{node.Uid}?enable={enabled}");
+        }
+        finally
+        {
+            Blocker.Hide();
+        }
+#endif
+    }
+
+    async Task<bool> Save(ExpandoObject model)
+    {
+#if (DEMO)
+        return true;
+#else
+        Blocker.Show();
+        this.StateHasChanged();
+
+        try
+        {
+            var saveResult = await HttpHelper.Post<ProcessingNode>($"{ApiUrl}", model);
+            if (saveResult.Success == false)
+            {
+                Toast.ShowError( saveResult.Body?.EmptyAsNull() ?? Translater.Instant("ErrorMessages.SaveFailed"));
+                return false;
+            }
+
+            int index = this.Data.FindIndex(x => x.Uid == saveResult.Data.Uid);
+            if (index < 0)
+                this.Data.Add(saveResult.Data);
+            else
+                this.Data[index] = saveResult.Data;
+            await this.Load(saveResult.Data.Uid);
+
+            return true;
+        }
+        finally
+        {
+            Blocker.Hide();
+            this.StateHasChanged();
+        }
+#endif
+    }
+
 }
--- Server/Controllers/NodeController.cs
+++ Server/Controllers/NodeController.cs
@@ -296,6 +296,24 @@ namespace FileFlows.Server.Controllers
             node.SignalrUrl = "flow";
             return node;
         }
+        
+        /// <summary>
+        /// Downloads the node from the server
+        /// </summary>
+        /// <returns>the node download zip</returns>
+        [HttpGet("download")]
+        public IActionResult Download()
+        {
+            string zipName = $"FileFlows-Node-{Globals.Version}.zip";
+            string file = Path.Combine(DirectoryHelper.BaseDirectory, "Server", "Nodes", zipName);
+            if (System.IO.File.Exists(file) == false)
+                return NotFound();
+
+            return new FileStreamResult(System.IO.File.OpenRead(file), "application/octet-stream")
+            {
+                FileDownloadName = zipName
+            };
+        }
     }
 
 }

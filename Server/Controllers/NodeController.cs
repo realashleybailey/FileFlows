803d74323486768381d23962c9587c18fbcbd901
John Andrews <john@reven.nz>
2022-05-18 16:32:39 +1200

    updated node download url

--- Server/Controllers/NodeController.cs
+++ Server/Controllers/NodeController.cs
@@ -301,24 +301,6 @@ namespace FileFlows.Server.Controllers
             node.SignalrUrl = "flow";
             return node;
         }
-        
-        /// <summary>
-        /// Downloads the node from the server
-        /// </summary>
-        /// <returns>the node download zip</returns>
-        [HttpGet("download")]
-        public IActionResult Download()
-        {
-            string zipName = $"FileFlows-Node-{Globals.Version}.zip";
-            string file = Path.Combine(DirectoryHelper.BaseDirectory, "Server", "Nodes", zipName);
-            if (System.IO.File.Exists(file) == false)
-                return NotFound();
-
-            return new FileStreamResult(System.IO.File.OpenRead(file), "application/octet-stream")
-            {
-                FileDownloadName = zipName
-            };
-        }
 
         /// <summary>
         /// Updates the last seen to now for a node

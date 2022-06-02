8ca00c601983419784543406199246669e733005
John Andrews <john@reven.nz>
2022-06-02 20:04:53 +1200

    fixing my sql stored procs

--- Server/Database/Scripts/MySql/GetLibraryFileOverview.sql
+++ Server/Database/Scripts/MySql/GetLibraryFileOverview.sql
@@ -2,81 +2,80 @@ DROP PROCEDURE IF EXISTS GetLibraryFileOverview;
 
 CREATE PROCEDURE GetLibraryFileOverview(IntervalIndex int)
 BEGIN
-
-declare libsDisabled as varchar(max)
-declare libsOutOfSchedule as varchar(max)
     
-declare cntDisabled as int
-declare cntOutOfSchedule as int
-declare cntUnprocessed as int
-declare cntProcessed as int
-declare cntProcessing as int
-declare cntFlowNotFound as int
-declare cntProcessingFailed as int
-declare cntDuplicate as int
-declare cntMappingIssue as int
+declare cntDisabled int;
+declare cntOutOfSchedule int;
+declare cntUnprocessed int;
+declare cntProcessed int;
+declare cntProcessing int;
+declare cntFlowNotFound int;
+declare cntProcessingFailed int;
+declare cntDuplicate int;
+declare cntMappingIssue int;
+declare libsDisabled text;
+declare libsOutOfSchedule text;
 
-set libsDisabled= (select STRING_AGG(cast([Uid] as varchar(36)),',') from DbObject
+set libsDisabled = (select STRING_AGG(Uid,',') from DbObject
 where type = 'FileFlows.Shared.Models.Library'
-  and JSON_EXTRACT([Data],'$.Enabled') = '0'
-)
+  and JSON_EXTRACT(Data,'$.Enabled') = '0'
+);
 
 
 set libsOutOfSchedule = (
-select STRING_AGG(cast([Uid] as varchar(36)),',') from DbObject
+select STRING_AGG(Uid,',') from DbObject
 where type = 'FileFlows.Shared.Models.Library'
-  and (JSON_EXTRACT([Data],'$.Schedule') = null or JSON_EXTRACT([Data],'$.Schedule') = '' or
-       substring(JSON_EXTRACT([Data], '$.Schedule'), IntervalIndex, 1) = '1')
-)
-
+  and (JSON_EXTRACT(Data,'$.Schedule') = null or JSON_EXTRACT(Data,'$.Schedule') = '' or
+       substring(JSON_EXTRACT(Data, '$.Schedule'), IntervalIndex, 1) = '1')
+);
 
 
 set cntDisabled = (
-    select count ([Uid]) from DbObject where type = 'FileFlows.Shared.Models.LibraryFile'
-    and JSON_EXTRACT([Data], '$.Status') = 0
-    and charindex(JSON_EXTRACT([Data], '$.Library.Uid'), libsDisabled) > 0
-    and charindex(JSON_EXTRACT([Data], '$.Library.Uid'), libsOutOfSchedule) < 1
-    )
+    select count (Uid) from DbObject where type = 'FileFlows.Shared.Models.LibraryFile'
+    and JSON_EXTRACT(Data, '$.Status') = 0
+    and charindex(JSON_EXTRACT(Data, '$.Library.Uid'), libsDisabled) > 0
+    and charindex(JSON_EXTRACT(Data, '$.Library.Uid'), libsOutOfSchedule) < 1
+    );
 
-set cntOutOfSchedule = (select count ([Uid]) from DbObject where type = 'FileFlows.Shared.Models.LibraryFile'
-    and JSON_EXTRACT([Data], '$.Status') = 0
-    and charindex(JSON_EXTRACT([Data], '$.Library.Uid'), libsDisabled) < 1
-    and charindex(JSON_EXTRACT([Data], '$.Library.Uid'), libsOutOfSchedule) > 1
-    )
+set cntOutOfSchedule = (select count (Uid) from DbObject where type = 'FileFlows.Shared.Models.LibraryFile'
+    and JSON_EXTRACT(Data, '$.Status') = 0
+    and charindex(JSON_EXTRACT(Data, '$.Library.Uid'), libsDisabled) < 1
+    and charindex(JSON_EXTRACT(Data, '$.Library.Uid'), libsOutOfSchedule) > 1
+    );
 
-set cntUnprocessed = (select count ([Uid]) from DbObject where type = 'FileFlows.Shared.Models.LibraryFile'
-    and JSON_EXTRACT([Data], '$.Status') = 0
-    and charindex(JSON_EXTRACT([Data], '$.Library.Uid'), libsDisabled) < 1
-    and charindex(JSON_EXTRACT([Data], '$.Library.Uid'), libsOutOfSchedule) < 1
-    )
+set cntUnprocessed = (select count (Uid) from DbObject where type = 'FileFlows.Shared.Models.LibraryFile'
+    and JSON_EXTRACT(Data, '$.Status') = 0
+    and charindex(JSON_EXTRACT(Data, '$.Library.Uid'), libsDisabled) < 1
+    and charindex(JSON_EXTRACT(Data, '$.Library.Uid'), libsOutOfSchedule) < 1
+    );
 
 
-set cntProcessed = (select count ([Uid]) from DbObject where type = 'FileFlows.Shared.Models.LibraryFile'
-    and JSON_EXTRACT([Data], '$.Status') = 1
-    )
+set cntProcessed = (select count (Uid) from DbObject where type = 'FileFlows.Shared.Models.LibraryFile'
+    and JSON_EXTRACT(Data, '$.Status') = 1
+    );
 
-set cntProcessing = (select count ([Uid]) from DbObject where type = 'FileFlows.Shared.Models.LibraryFile'
-    and JSON_EXTRACT([Data], '$.Status') = 2
-    )
+set cntProcessing = (select count (Uid) from DbObject where type = 'FileFlows.Shared.Models.LibraryFile'
+    and JSON_EXTRACT(Data, '$.Status') = 2
+    );
 
-set cntFlowNotFound = (select count ([Uid]) from DbObject where type = 'FileFlows.Shared.Models.LibraryFile'
-    and JSON_EXTRACT([Data], '$.Status') = 3
-    )
+set cntFlowNotFound = (select count (Uid) from DbObject where type = 'FileFlows.Shared.Models.LibraryFile'
+    and JSON_EXTRACT(Data, '$.Status') = 3
+    );
 
-set cntProcessingFailed = (select count ([Uid]) from DbObject where type = 'FileFlows.Shared.Models.LibraryFile'
-    and JSON_EXTRACT([Data], '$.Status') = 4
-    )
+set cntProcessingFailed = (select count (Uid) from DbObject where type = 'FileFlows.Shared.Models.LibraryFile'
+    and JSON_EXTRACT(Data, '$.Status') = 4
+    );
 
-set cntDuplicate = (select count ([Uid]) from DbObject where type = 'FileFlows.Shared.Models.LibraryFile'
-    and JSON_EXTRACT([Data], '$.Status') = 5
-    )
+set cntDuplicate = (select count (Uid) from DbObject where type = 'FileFlows.Shared.Models.LibraryFile'
+    and JSON_EXTRACT(Data, '$.Status') = 5
+    );
 
-set cntMappingIssue = (select count ([Uid]) from DbObject where type = 'FileFlows.Shared.Models.LibraryFile'
-    and JSON_EXTRACT([Data], '$.Status') = 6
-    )
+set cntMappingIssue = (select count (Uid) from DbObject where type = 'FileFlows.Shared.Models.LibraryFile'
+    and JSON_EXTRACT(Data, '$.Status') = 6
+    );
 
 select cntDisabled as Disabled, cntOutOfSchedule as OutOfSchedule, cntUnprocessed as Unprocessed,
-    cntProcessed as Processed, cntProcessing as Processing, cntFlowNotFound as FlowNotFound,
-    cntProcessingFailed as ProcessingFailed, cntDuplicate as Duplicate, cntMappingIssue as MappingIssue
+       cntProcessed as Processed, cntProcessing as Processing, cntFlowNotFound as FlowNotFound,
+       cntProcessingFailed as ProcessingFailed, cntDuplicate as Duplicate, cntMappingIssue as MappingIssue;
+
 
 END;
--- Server/Database/Scripts/MySql/GetLibraryFiles.sql
+++ Server/Database/Scripts/MySql/GetLibraryFiles.sql
@@ -3,42 +3,43 @@ DROP PROCEDURE IF EXISTS GetLibraryFiles;
 CREATE PROCEDURE GetLibraryFiles(IntervalIndex int, Status int)
 BEGIN
     
-    declare libsDisabled as varchar(max)
-    declare libsOutOfSchedule as varchar(max)
+    declare libsDisabled text;
+    declare libsOutOfSchedule text;
     
     if Status > 0 then
         select * from DbObject where type = 'FileFlows.Shared.Models.LibraryFile'
-        and JSON_EXTRACT([Data], '$.Status') = Status
+                             and JSON_EXTRACT(Data, '$.Status') = Status;
+    
     else
     
-        select libsDisabled=STRING_AGG(cast([Uid] as varchar(36)),',') from DbObject
-        where type = 'FileFlows.Shared.Models.Library'
-            and JSON_EXTRACT([Data],'$.Enabled') = '0'
-        
-        
-        select libsOutOfSchedule=STRING_AGG(cast([Uid] as varchar(36)),',') from DbObject
-        where type = 'FileFlows.Shared.Models.Library'
-            and (JSON_EXTRACT([Data],'$.Schedule') = null or JSON_EXTRACT([Data],'$.Schedule') = '' or
-            substring(JSON_EXTRACT([Data], '$.Schedule'), IntervalIndex, 1) = '1')
-        
-        if Status = -2 then -- disabled
-            select * from DbObject where type = 'FileFlows.Shared.Models.LibraryFile'
-            and JSON_EXTRACT([Data], '$.Status') = 0
-            and charindex(JSON_EXTRACT([Data], '$.Library.Uid'), libsDisabled) > 0
-            and charindex(JSON_EXTRACT([Data], '$.Library.Uid'), libsOutOfSchedule) < 1
-        
-        else if Status = -1 then -- out of schedule
-            select * from DbObject where type = 'FileFlows.Shared.Models.LibraryFile'
-            and JSON_EXTRACT([Data], '$.Status') = 0
-            and charindex(JSON_EXTRACT([Data], '$.Library.Uid'), libsDisabled) < 1
-            and charindex(JSON_EXTRACT([Data], '$.Library.Uid'), libsOutOfSchedule) > 1
-        
-        else
-            select * from DbObject where type = 'FileFlows.Shared.Models.LibraryFile'
-            and JSON_EXTRACT([Data], '$.Status') = 0
-            and charindex(JSON_EXTRACT([Data], '$.Library.Uid'), libsDisabled) < 1
-            and charindex(JSON_EXTRACT([Data], '$.Library.Uid'), libsOutOfSchedule) < 1
-        end if;
+    select libsDisabled=STRING_AGG(Uid,',') from DbObject
+    where type = 'FileFlows.Shared.Models.Library'
+      and JSON_EXTRACT(Data,'$.Enabled') = '0';
+    
+    
+    select libsOutOfSchedule=STRING_AGG(Uid,',') from DbObject
+    where type = 'FileFlows.Shared.Models.Library'
+      and (JSON_EXTRACT(Data,'$.Schedule') = null or JSON_EXTRACT(Data,'$.Schedule') = '' or
+           substring(JSON_EXTRACT(Data, '$.Schedule'), IntervalIndex, 1) = '1');
+    
+    if Status = -2 then -- disabled
+    select * from DbObject where type = 'FileFlows.Shared.Models.LibraryFile'
+                             and JSON_EXTRACT(Data, '$.Status') = 0
+                             and charindex(JSON_EXTRACT(Data, '$.Library.Uid'), libsDisabled) > 0
+                             and charindex(JSON_EXTRACT(Data, '$.Library.Uid'), libsOutOfSchedule) < 1;
+    
+    elseif Status = -1 then -- out of schedule
+    select * from DbObject where type = 'FileFlows.Shared.Models.LibraryFile'
+                             and JSON_EXTRACT(Data, '$.Status') = 0
+                             and charindex(JSON_EXTRACT(Data, '$.Library.Uid'), libsDisabled) < 1
+                             and charindex(JSON_EXTRACT(Data, '$.Library.Uid'), libsOutOfSchedule) > 1;
+    
+    else
+    select * from DbObject where type = 'FileFlows.Shared.Models.LibraryFile'
+                             and JSON_EXTRACT(Data, '$.Status') = 0
+                             and charindex(JSON_EXTRACT(Data, '$.Library.Uid'), libsDisabled) < 1
+                             and charindex(JSON_EXTRACT(Data, '$.Library.Uid'), libsOutOfSchedule) < 1;
+    end if;
     end if;
 
-GO
+END;

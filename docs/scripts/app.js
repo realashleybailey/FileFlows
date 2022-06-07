cb701d586a90b957ca4ee437fc4f66248e26f1cf
John Andrews <john@reven.nz>
2022-06-08 01:15:47 +1200

    docs

--- docs/scripts/app.js
+++ docs/scripts/app.js
@@ -1,12 +1,18 @@
 document.addEventListener("DOMContentLoaded", function()
 {
-
+    var checkboxes = document.querySelectorAll('.side-bar input[type=checkbox]');
+    for(let chk of checkboxes){
+        let id = chk.id;
+        if(localStorage.getItem('collapse_' + id) == 1){
+            chk.checked = true;
+        }
+    }
 });
 
 function toggleCollapse(event){
     let id = event.target.id;
     console.log('event.target.id: ', event.target.id);
     console.log('event.target.checked: ', event.target.checked);
-    localStorage.setItem('collapse_' + id, event.target.checked);
+    localStorage.setItem('collapse_' + id, event.target.checked ? 1 : 0);
 
 }

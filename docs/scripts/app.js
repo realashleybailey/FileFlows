70dfea5564915695d18abf52d9a6f6dd113b48d0
John Andrews <john@reven.nz>
2022-06-08 08:58:36 +1200

    docs

--- docs/scripts/app.js
+++ docs/scripts/app.js
@@ -9,10 +9,10 @@ document.addEventListener("DOMContentLoaded", function()
 function checkToggle(chk)
 {
     let id = chk.id;
-    if(!!localStorage.getItem('collapse_' + id)){
+    if(localStorage.getItem('collapse_' + id) == true){
         chk.checked = true;
-        console.log('toggle2:', id);
-        console.log('collapse2: ', localStorage.getItem('collapse_' + id));
+        console.log('toggle3:', id);
+        console.log('collapse3: ', localStorage.getItem('collapse_' + id));
 
         let parent = chk.parentNode.closest('input[type=checkbox]');
         if(parent)
@@ -22,8 +22,16 @@ function checkToggle(chk)
 
 function toggleCollapse(event){
     let id = event.target.id;
-    console.log('event.target.id: ', event.target.id);
-    console.log('event.target.checked: ', event.target.checked);
-    localStorage.setItem('collapse_' + id, event.target.checked ? 1 : 0);
-
+    let chk = event.target;
+    console.log('chk.id: ', chk.id);
+    console.log('chk.checked: ', chk.checked);
+    localStorage.setItem('collapse_' + id, chk.checked ? 1 : 0);
+    
+    // close any below this one if checked
+    if(event.target.checked == false){
+        for(let sub of chk.querySelectorAll('input[type=checkbox]')){
+            if(sub.checked)
+                toggleCollapse(sub);
+        }
+    }
 }

f56cb04985a47acecfbc1f7a69d92a3ecf67a390
John Andrews <john@reven.nz>
2022-06-09 23:18:32 +1200

    docs

--- docs/_plugins/BasicNodes/nodes/Function.md
+++ docs/_plugins/BasicNodes/nodes/Function.md
@@ -7,9 +7,6 @@ plugin: Basic Nodes
 toc: true
 ---
 
-* auto-gen TOC:
-{:toc}
-
 {% include node.html input=1 outputs=1 icon="fas fa-code" name="Function" type="Logic" %}
 
 
--- docs/scripts/app.js
+++ docs/scripts/app.js
@@ -93,9 +93,6 @@ function captureLinks(container) {
     var links = container.querySelectorAll('a');
     for(let a of links) 
     {
-        if(a.closest('#markdown-toc'))
-            continue;
-            
         if(a.closest('.toc')){
             a.addEventListener('click', function(event) {
                 event.stopPropagation();

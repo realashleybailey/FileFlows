40f4171895d94b8ea87f1d2d9055927580afeaa7
John Andrews <john@reven.nz>
2022-06-19 15:25:03 +1200

    fixing issue with GetShrinkageData.sql using only database named FileFlows

--- Server/Database/Scripts/MySql/GetShrinkageData.sql
+++ Server/Database/Scripts/MySql/GetShrinkageData.sql
@@ -8,8 +8,8 @@ select
     count(dbFile.Uid) as Items,
     sum(JSON_EXTRACT(dbFile.Data, '$.OriginalSize')) as OriginalSize,
     sum(JSON_EXTRACT(dbFile.Data, '$.FinalSize')) as FinalSize
-from FileFlows.DbObject dbFile
-         inner join FileFlows.DbObject dbLib on dbLib.Uid = JSON_UNQUOTE(JSON_EXTRACT(dbFile.Data, '$.Library.Uid'))
+from DbObject dbFile
+         inner join DbObject dbLib on dbLib.Uid = JSON_UNQUOTE(JSON_EXTRACT(dbFile.Data, '$.Library.Uid'))
 where
         dbFile.Type = 'FileFlows.Shared.Models.LibraryFile'
   and

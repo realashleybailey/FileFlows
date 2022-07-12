93277fd4b7451509ebb06f3b04e9e9d7ba92ebf9
John Andrews <john@reven.nz>
2022-07-12 15:39:15 +1200

    FF-147 - adding logging to ObjectReferenceUpdater.cs

--- Server/Workers/ObjectReferenceUpdater.cs
+++ Server/Workers/ObjectReferenceUpdater.cs
@@ -8,7 +8,7 @@ namespace FileFlows.Server.Workers;
 /// </summary>
 public class ObjectReferenceUpdater:Worker
 {
-    
+    private static bool IsRunning = false;
     /// <summary>
     /// Creates a new instance of the Object Reference Updater 
     /// </summary>
@@ -26,43 +26,59 @@ public class ObjectReferenceUpdater:Worker
     /// </summary>
     internal void Run()
     {
-        var libFileController = new LibraryFileController();
-        var libraryController = new LibraryController();
-        var libFiles = libFileController.GetAll(null).Result;
-        var libraries = libraryController.GetAll().Result;
-        var flows = new FlowController().GetAll().Result;
-
-        var dictLibraries = libraries.ToDictionary(x => x.Uid, x => x.Name);
-        var dictFlows = flows.ToDictionary(x => x.Uid, x => x.Name);
-
-        foreach (var lf in libFiles)
+        if (IsRunning)
+            return;
+        IsRunning = true;
+        try
         {
-            bool changed = false;
-            if (dictLibraries.ContainsKey(lf.Library.Uid) && lf.Library.Name != dictLibraries[lf.Library.Uid])
+            var libFileController = new LibraryFileController();
+            var libraryController = new LibraryController();
+            DateTime start = DateTime.Now;
+            var libFiles = libFileController.GetAll(null).Result;
+            var libraries = libraryController.GetAll().Result;
+            var flows = new FlowController().GetAll().Result;
+
+            var dictLibraries = libraries.ToDictionary(x => x.Uid, x => x.Name);
+            var dictFlows = flows.ToDictionary(x => x.Uid, x => x.Name);
+            
+            Logger.Instance.ILog("Time Taken to prepare for ObjectReference rename: "+ DateTime.Now.Subtract(start));
+
+            foreach (var lf in libFiles)
             {
-                lf.Library.Name = dictLibraries[lf.Library.Uid];
-                Logger.Instance.ILog($"Updating Library name reference '{lf.Flow.Name}' in file: {lf.Name}");
-                changed = true;
-            }
-            if (lf.Flow != null && lf.Flow.Uid != Guid.Empty && dictFlows.ContainsKey(lf.Flow.Uid) && lf.Flow.Name != dictFlows[lf.Flow.Uid])
-            {
-                lf.Flow.Name = dictFlows[lf.Flow.Uid];
-                Logger.Instance.ILog($"Updating Flow name reference '{lf.Flow.Name}' in file: {lf.Name}");
-                changed = true;
+                bool changed = false;
+                if (dictLibraries.ContainsKey(lf.Library.Uid) && lf.Library.Name != dictLibraries[lf.Library.Uid])
+                {
+                    lf.Library.Name = dictLibraries[lf.Library.Uid];
+                    Logger.Instance.ILog($"Updating Library name reference '{lf.Flow.Name}' in file: {lf.Name}");
+                    changed = true;
+                }
+
+                if (lf.Flow != null && lf.Flow.Uid != Guid.Empty && dictFlows.ContainsKey(lf.Flow.Uid) &&
+                    lf.Flow.Name != dictFlows[lf.Flow.Uid])
+                {
+                    lf.Flow.Name = dictFlows[lf.Flow.Uid];
+                    Logger.Instance.ILog($"Updating Flow name reference '{lf.Flow.Name}' in file: {lf.Name}");
+                    changed = true;
+                }
+
+                if (changed)
+                    libFileController.Update(lf).Wait();
             }
 
-            if (changed)
-                libFileController.Update(lf).Wait();
+            foreach (var lib in libraries)
+            {
+                if (dictFlows.ContainsKey(lib.Flow.Uid) && lib.Flow.Name != dictFlows[lib.Flow.Uid])
+                {
+                    lib.Flow.Name = dictFlows[lib.Flow.Uid];
+                    Logger.Instance.ILog($"Updating Flow name reference '{lib.Flow.Name}' in library: {lib.Name}");
+                    libraryController.Update(lib).Wait();
+                }
+            }
+            Logger.Instance.ILog("Time Taken to complete for ObjectReference rename: "+ DateTime.Now.Subtract(start));
         }
-
-        foreach (var lib in libraries)
+        finally
         {
-            if (dictFlows.ContainsKey(lib.Flow.Uid) && lib.Flow.Name != dictFlows[lib.Flow.Uid])
-            {
-                lib.Flow.Name = dictFlows[lib.Flow.Uid];
-                Logger.Instance.ILog($"Updating Flow name reference '{lib.Flow.Name}' in library: {lib.Name}");
-                libraryController.Update(lib).Wait();
-            }
+            IsRunning = false;
         }
     }
 }

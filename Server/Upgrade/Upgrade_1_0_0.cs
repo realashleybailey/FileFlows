98463eeb05ca8511989f59ef74041cd812cf7486
John Andrews <john@reven.nz>
2022-08-05 17:55:35 +1200

    fixing upgrade to 1.0.0

--- Server/Upgrade/Upgrade_1_0_0.cs
+++ Server/Upgrade/Upgrade_1_0_0.cs
@@ -1,8 +1,4 @@
-using System.Text.RegularExpressions;
-using FileFlows.Server.Controllers;
-using FileFlows.Server.Database.Managers;
 using FileFlows.Server.Helpers;
-using FileFlows.Server.Workers;
 using FileFlows.Shared.Models;
 
 namespace FileFlows.Server.Upgrade;
@@ -21,11 +17,12 @@ public class Upgrade_1_0_0
         Logger.Instance.ILog("Upgrade running, running 1.0.0 upgrade script");
         RenameToolsToVariables();
         MoveUserScripts();
-        MoveSystemScripts();
+        RemoveOldSystemScripts();
     }
 
     private void RenameToolsToVariables()
     {
+        Logger.Instance.ILog("Renaming Tools to Variables");
         var manager = DbHelper.GetDbManager();
         manager.Execute(
             "update DbObject set Type = 'FileFlows.Shared.Models.Variable', Data = replace(Data, 'Path', 'Value') where Type = 'FileFlows.Shared.Models.Tool'", null).Wait();
@@ -35,6 +32,7 @@ public class Upgrade_1_0_0
 
     private void MoveUserScripts()
     {
+        Logger.Instance.ILog("Moving User Scripts");
         var oldDir = new DirectoryInfo(Path.Combine(DirectoryHelper.ScriptsDirectory, "User"));
         if (oldDir.Exists == false)
             return;
@@ -45,31 +43,13 @@ public class Upgrade_1_0_0
         oldDir.Delete();
     }
 
-    private void MoveSystemScripts()
+    private void RemoveOldSystemScripts()
     {
+        Logger.Instance.ILog("Removing old system scripts");
         var oldDir = new DirectoryInfo(Path.Combine(DirectoryHelper.ScriptsDirectory, "System"));
         if (oldDir.Exists == false)
             return;
-        // system scripts are now repository scripts, and they contain a path on the first line so they can be updated
-        foreach (var file in oldDir.GetFiles())
-        {
-            string path = "Flow/";
-            if (file.Name.StartsWith("File -"))
-                path += "File/";
-            else if (file.Name.StartsWith("NVIDIA -"))
-                path += "Hardware/";
-            else if (file.Name.StartsWith("7Zip -"))
-                path += "Utilities/";
-            else if (file.Name.StartsWith("Video -"))
-                path += "Video/";
-            else
-            {
-                Logger.Instance.WLog("Unknown system script, cannot upgrade this script: " + file.FullName);
-                continue;
-            }
-            string content = "// path: " + path + "\n\n" + File.ReadAllText(file.FullName);
-            File.WriteAllText(Path.Combine(DirectoryHelper.ScriptsDirectoryFlow, file.Name), content);
-        }
-        oldDir.Delete();
+        foreach(var file in oldDir.GetFiles("*.js"))
+            file.Delete();
     }
 }

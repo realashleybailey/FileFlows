578dcbc9740f32150eed8a5642c3aa21aef80fce
John Andrews <john@reven.nz>
2022-09-29 08:32:50 +1300

    FF-337 - fixing upgrade issue in 0.9.0

--- Server/Upgrade/Upgrade_0_9_0.cs
+++ Server/Upgrade/Upgrade_0_9_0.cs
@@ -72,8 +72,18 @@ public class Upgrade_0_9_0
     /// </summary>
     private void AddStatisticsTable()
     {
-        if(DbHelper.GetDbManager() is SqliteDbManager sqlite)
-            sqlite.Execute(SqliteCreateDbStatisticTableScript, new object[]{}).Wait();
+        if (DbHelper.GetDbManager() is SqliteDbManager sqlite)
+        {
+            try
+            {
+                sqlite.Execute($"drop table {nameof(DbStatistic)}",new object[] { }).Wait();
+                sqlite.Execute(SqliteCreateDbStatisticTableScript, new object[] { }).Wait();
+            }
+            catch (Exception)
+            {
+                // FF-337.   easiest to just catch this error and ignore it.
+            }
+        }
     }
 
     private void ExportScripts()
